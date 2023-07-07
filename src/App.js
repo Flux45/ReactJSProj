e5f423a45ac9da919bf397f74aee237302aaf138
Flux45 <116813328+Flux45@users.noreply.github.com>
2023-07-07 18:38:06 +0530

    Offline
    Dark Mode Functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="container my-3">
       
       <TextForm heading="Enter the text to analyze"/>
+      <About></About>
     </div>
   </>
     
